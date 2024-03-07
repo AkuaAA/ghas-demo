78940a5723c82b4590a0857876cc4e6cc63301cd
Akua Apeagyei <t-aapeagyei@microsoft.com>
2024-03-07 11:41:18 +0000

    bad password

--- demo.py
+++ demo.py
@@ -1,3 +1,5 @@
+import secrets
+
 class User:
     def __init__(self, username, password):
         self.username = username
@@ -5,6 +7,9 @@ class User:
 
 class LoginSystem:
     def __init__(self):
+         # This is a hard-coded password, which is a bad practice. 
+
+        self.user = User("admin", "password123") 
 
     def login(self, username, password):
         if username == self.user.username and password == self.user.password:
@@ -17,3 +22,5 @@ login_system = LoginSystem()
 
 # Try to login
 login_system.login("admin", "password123")
+
+SENSITIVE_TOKEN = secrets.token_hex(16)  # Generate a random hexadecimal string (32 characters)
